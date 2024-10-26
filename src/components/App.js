5cef67dcb91212b5d01f331ddfc0ab311f2200e8
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-10-27 02:21:33 +0300

    added functionality of the pages

--- src/components/App.js
+++ src/components/App.js
@@ -19,8 +19,9 @@ export const KEY = "64100630";
 export default function App() {
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const [page, setPage] = useState(1);
 
-  const { movies, isLoading, error } = useMovies(query);
+  const { movies, isLoading, error } = useMovies(query, page);
 
   const [watched, setWatched] = useLocalStorage([], "watched");
 
@@ -42,15 +43,35 @@ export default function App() {
 
   return (
     <>
-      <NavBar setQuery={setQuery} query={query} movies={movies} />
+      <NavBar
+        setQuery={setQuery}
+        query={query}
+        movies={movies}
+        setPage={setPage}
+      />
 
       <Main>
         <ListBox>
           {isLoading && <Loader />}
           {!isLoading && !error && (
-            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+            <>
+              <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+            </>
           )}
           {error && <ErrorMessage message={error} />}
+          <div className="buttons">
+            {page !== 1 ? (
+              <button onClick={() => setPage(page - 1)}>&larr;</button>
+            ) : (
+              <button className="placeholder"></button>
+            )}
+            {(page !== 1 || movies.length !== 0) && <p>page {page}</p>}
+            {movies.length === 10 ? (
+              <button onClick={() => setPage(page + 1)}>&rarr;</button>
+            ) : (
+              <button className="placeholder"></button>
+            )}
+          </div>
         </ListBox>
 
         <ListBox>
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import { useKey } from "./useKey";
 
-export default function Search({ setQuery, query }) {
+export default function Search({ setQuery, query, setPage }) {
   const inputEl = useRef(null);
   useKey(
     "Enter",
@@ -22,7 +22,10 @@ export default function Search({ setQuery, query }) {
       type="text"
       placeholder="Search movies..."
       value={query}
-      onChange={(e) => setQuery(e.target.value)}
+      onChange={(e) => {
+        setQuery(e.target.value);
+        setPage(1);
+      }}
       ref={inputEl}
     />
   );
