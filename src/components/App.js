6c768481a3eefa32ab95d2a7f903edb5f12dfdc7
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-10-27 02:23:28 +0300

    fixed a bug with buttons, when movies are loading

--- src/components/App.js
+++ src/components/App.js
@@ -59,19 +59,21 @@ export default function App() {
             </>
           )}
           {error && <ErrorMessage message={error} />}
-          <div className="buttons">
-            {page !== 1 ? (
-              <button onClick={() => setPage(page - 1)}>&larr;</button>
-            ) : (
-              <button className="placeholder"></button>
-            )}
-            {(page !== 1 || movies.length !== 0) && <p>page {page}</p>}
-            {movies.length === 10 ? (
-              <button onClick={() => setPage(page + 1)}>&rarr;</button>
-            ) : (
-              <button className="placeholder"></button>
-            )}
-          </div>
+          {!isLoading && (
+            <div className="buttons">
+              {page !== 1 ? (
+                <button onClick={() => setPage(page - 1)}>&larr;</button>
+              ) : (
+                <button className="placeholder"></button>
+              )}
+              {(page !== 1 || movies.length !== 0) && <p>page {page}</p>}
+              {movies.length === 10 ? (
+                <button onClick={() => setPage(page + 1)}>&rarr;</button>
+              ) : (
+                <button className="placeholder"></button>
+              )}
+            </div>
+          )}
         </ListBox>
 
         <ListBox>
