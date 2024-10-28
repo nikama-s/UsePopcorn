cb1e1a69911bb961e38dff554f786ec3f930d5ba
Veronika Maslo <maslo.veronika@lll.kpi.ua>
2024-10-28 23:28:19 +0200

    deleted an unneeded function for pagination

--- src/components/App.js
+++ src/components/App.js
@@ -42,24 +42,6 @@ export default function App() {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
 
-  function renderPagination() {
-    return (
-      <div className="buttons">
-        {page > 1 ? (
-          <button onClick={() => setPage(page - 1)}>&larr;</button>
-        ) : (
-          <button className="placeholder"></button>
-        )}
-        {(page > 1 || movies.length > 0) && <p>Page {page}</p>}
-        {movies.length === 10 ? (
-          <button onClick={() => setPage(page + 1)}>&rarr;</button>
-        ) : (
-          <button className="placeholder"></button>
-        )}
-      </div>
-    );
-  }
-
   return (
     <>
       <NavBar
